d892a8247acac64770de49a48c753ea2b256dfa5
Lawrence Siden <lsiden@gmail.com>
2017-10-09 12:18:59 -0400

    Refactoring package.json scripts for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist',
+        publicPath: 'dist/',
         library: 'SelectCurrency',
         libraryTarget: 'umd',
     },
