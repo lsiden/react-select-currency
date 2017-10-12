bb985229b67ca2f9ab43e732a463bb0a56799123
Lawrence Siden <lsiden@gmail.com>
2017-10-11 21:47:34 -0400

    prepare-gh-pages.js

---
+++ prepare-gh-pages.js
@@ -0,0 +1,31 @@
+const path=require('path')
+const fs=require('fs-extra')
+const {exec} = require('child_process')
+const ghPagesPath = './gh-pages'
+
+function toPromise(func) {
+    return function() {
+        const args = [].slice.call(arguments)
+        return new Promise(function(resolve, reject) {
+            args.push(function(err) {
+                err && reject() || resolve()
+            })
+            func.apply(null, args)
+        })
+    }
+}
+
+(function(dest) {
+    fs.remove(dest)
+    .then(function() {
+        return fs.mkdir(dest)
+    }).then(function() {
+        return Promise.all([
+            fs.copy('dist', path.join(dest, 'dist')),
+            fs.copy('./demo.html', path.join(dest, 'index.html')),
+            toPromise(exec)('yarn babel demo.jsx -o ' + path.join(dest, 'index.js')),
+        ])
+    }).catch(function(err) {
+        console.error(err)
+    })
+})(ghPagesPath)
